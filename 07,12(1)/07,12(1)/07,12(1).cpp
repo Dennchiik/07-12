ca7dca92f95b0888485f7bab1a19e644c1702f04
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-12-10 18:09:00 +0200

    Add files via upload

---
+++ 07,12(1)/07,12(1)/07,12(1).cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+class DigitalCounter {
+private:
+    int value;
+    int minValue;
+    int maxValue;
+
+public:
+    DigitalCounter(int min, int max) : minValue(min), maxValue(max) {
+        value = min;
+    }
+
+    void setValue(int val) {
+        if (val >= minValue && val <= maxValue) {
+            value = val;
+        }
+        else {
+            std::cout << "Значение должно быть в диапазоне от " << minValue << " до " << maxValue << std::endl;
+        }
+    }
+
+    void increment() {
+        if (value < maxValue) {
+            value++;
+        }
+        else {
+            value = minValue;
+        }
+    }
+
+    int getValue() const {
+        return value;
+    }
+
+    void Run(int iterations) {
+        for (int i = 0; i < iterations; ++i) {
+            increment();
+            std::cout << "Текущее значение счетчика: " << getValue() << std::endl;
+        }
+    }
+};
+
+int main() {
+    DigitalCounter counter(0, 100);
+    counter.setValue(50);
+    counter.increment();
+    std::cout << "Текущее значение счетчика: " << counter.getValue() << std::endl;
+    counter.Run(5);
+
+    return 0;
+}
---
+++ 07,12(2)/07,12(2)/07,12(2).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+class Elevator {
+private:
+    int currentFloor;
+    int totalFloors;
+
+public:
+    Elevator(int floors) : totalFloors(floors) {
+        currentFloor = 1;
+    }
+
+    void moveToFloor(int floor) {
+        if (floor >= 1 && floor <= totalFloors) {
+            std::cout << "Лифт движется с " << currentFloor << " этажа на " << floor << " этаж" << std::endl;
+            currentFloor = floor;
+        }
+        else {
+            std::cout << "Такого этажа нет в доме" << std::endl;
+        }
+    }
+
+    int getCurrentFloor() const {
+        return currentFloor;
+    }
+};
+
+int main() {
+    Elevator lift(10);
+    lift.moveToFloor(5);
+    std::cout << "Текущий этаж лифта: " << lift.getCurrentFloor() << std::endl;
+
+    return 0;
+}
